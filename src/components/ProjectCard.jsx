efaf2e312fe8864473b8201ee42c0d9605d88031
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-08-07 12:53:38 -0300

    Cambaindo diseño de ProyectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,22 +4,34 @@ const ProjectCard = ({
     title,
     tags,
     projectLink,
+    githubLink,
+    videoLink,
     classes
 }) => {
     return (
         <div className={"relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/5 transition-colors"}>
-            <figure className="img-box aspect-square rounded-lg mb-4">
+            <figure className="relative img-box aspect-square rounded-lg mb-4">
                 <img
                     src={imgSrc}
                     alt={title}
                     loading="lazy"
                     className="img-cover" />
+                    {videoLink &&  <a
+                    href={videoLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="absolute top-2 right-2 bg-white text-black rounded-full w-9 h-9 grid place-items-center shadow-lg hover:bg-zinc-600 hover:text-red-400 transition"
+                    title="Ver video del proyecto"
+                    >
+                        <span className="material-symbols-rounded">video_library</span>
+
+                    </a>}
+                  
             </figure>
+
             <div className="flex items-center justify-between gap-4">
                 <div>
-                    <h3 className="title-1 mb-3">
-                        {title}
-                    </h3>
+                    <h3 className="title-1 mb-3">{title}</h3>
                     <div className="flex flex-wrap items-center gap-2">
                         {tags.map((label, key) => (
                             <span
@@ -30,19 +42,28 @@ const ProjectCard = ({
                         ))}
                     </div>
                 </div>
-                <div className="w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0">
-                    <span
-                        className="material-symbols-rounded"
-                        aria-hidden="true">
-                        arrow_outward
-                    </span>
+
+                <div className="flex flex-col gap-2 shrink-0">
+                    <a
+                        href={projectLink}
+                        target="_blank"
+                        className="w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950">
+                        <span className="material-symbols-rounded" aria-hidden="true">
+                            arrow_outward
+                        </span>
+                    </a>
+                    <a
+                        href={githubLink}
+                        target="_blank"
+                        className="w-11 h-11 rounded-lg grid place-items-center bg-neutral-100 text-zinc-900">
+                        <span className="material-symbols-rounded" aria-hidden="true">
+                            code
+                        </span>
+                    </a>
                 </div>
             </div>
-            <a
-                href={projectLink}
-                target="_blank"
-                className="absolute inset-0"></a>
         </div>
+
     )
 }
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,49 +5,65 @@ const works = [
     imgSrc: '/images/allhardward.jpg',
     title: 'Full stack allhardward app',
     tags: ['E-commerce', 'MVC', 'Laravel'],
-    projectLink: 'https://github.com/Jose-Q91/Tienda-Online-All-Hardward-con-Laravel'
+    projectLink: 'https://github.com/Jose-Q91/Tienda-Online-All-Hardward-con-Laravel',
+    githubLink: 'https://github.com/Jose-Q91/Tienda-Online-All-Hardward-con-Laravel',
+    videoLink: ''
   },
   {
     imgSrc: '/images/healthy.jpg',
     title: 'Full stack healthy app',
     tags: ['E-commerce', 'MVC', 'Flask'],
-    projectLink: 'https://github.com/Jose-Q91/HealthyBite-con-Flask'
+    projectLink: 'https://github.com/Jose-Q91/HealthyBite-con-Flask',
+    githubLink: 'https://github.com/Jose-Q91/HealthyBite-con-Flask',
+    videoLink: ''
   },
   {
     imgSrc: '/images/oradores.jpg',
     title: 'Full stack oradores app',
     tags: ['Web-site', 'MVC', 'Java'],
-    projectLink: 'https://github.com/Jose-Q91/App-Web-con-Java'
+    projectLink: 'https://joseluisx10.github.io/CaC-TP-Integrador/#seccion-encabezado',
+    githubLink: 'https://github.com/Jose-Q91/App-Web-con-Java',
+    videoLink: 'https://www.dailymotion.com/video/x8mjvky'
   },
   {
     imgSrc: '/images/ecommerce.jpg',
     title: 'E-Commerce Mario',
     tags: ['E-commerce', 'React + TypeScript'],
-    projectLink: 'https://github.com/Jose-Q91/E-commerce-React'
+    projectLink: 'https://github.com/Jose-Q91/E-commerce-React',
+    githubLink: 'https://github.com/Jose-Q91/E-commerce-React',
+    videoLink: ''
   },
   {
     imgSrc: '/images/startgame.jpg',
     title: 'Startgame website',
     tags: ['API', 'Development', 'NodeJS'],
-    projectLink: 'https://github.com/joseluisx10/Proyecto-gameStart-con-NodeJS'
+    projectLink: 'https://github.com/joseluisx10/Proyecto-gameStart-con-NodeJS',
+    githubLink: 'https://github.com/joseluisx10/Proyecto-gameStart-con-NodeJS',
+    videoLink: ''
   },
   {
     imgSrc: '/images/appeventos.jpg',
     title: 'Eventos Musicales App',
     tags: ['Web-site', 'MVC', 'PHP'],
-    projectLink: 'https://github.com/Jose-Q91/Aplicacion-Web-de-Eventos-Musicales-con-PHP'
+    projectLink: 'https://github.com/Jose-Q91/Aplicacion-Web-de-Eventos-Musicales-con-PHP',
+    githubLink: 'https://github.com/Jose-Q91/Aplicacion-Web-de-Eventos-Musicales-con-PHP',
+    videoLink: ''
   },
   {
     imgSrc: '/images/allhardware.jpg',
     title: 'Full Stack AllHardware App',
     tags: ['E-commerce', 'MVC', 'NodeJS'],
-    projectLink: 'https://github.com/matiasniedermaier/grupo_4_AllHardware'
+    projectLink: 'https://github.com/matiasniedermaier/grupo_4_AllHardware',
+    githubLink: 'https://github.com/matiasniedermaier/grupo_4_AllHardware',
+    videoLink: ''
   },
   {
     imgSrc: '/images/app-cajero.jpg',
     title: 'Cajero Automático',
     tags: ['MVC', 'Java', 'Desktop'],
-    projectLink: 'https://github.com/Jose-Q91/Cajero-Autom-tico'
+    projectLink: 'https://github.com/Jose-Q91/Cajero-Autom-tico',
+    githubLink: 'https://github.com/Jose-Q91/Cajero-Autom-tico',
+    videoLink: ''
   },
 ];
 
@@ -62,13 +78,15 @@ const Work = () => {
                 Mi Portafolio
             </h2>
             <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-                {works.map(({imgSrc, title, tags, projectLink}, key)=> (
+                {works.map(({imgSrc, title, tags, projectLink, githubLink, videoLink}, key)=> (
                     <ProjectCard 
                     key={key}
                     imgSrc={imgSrc}
                     title= {title} 
                     tags = {tags}
-                    projectLink={projectLink}/>
+                    projectLink={projectLink}
+                    githubLink={githubLink}
+                    videoLink={videoLink}/>
                 ))}
             </div>
         </div>
